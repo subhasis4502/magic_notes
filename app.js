cfad124d5ddd9ee0bb0e5d09c46ce46aafc51b80
subhasis4502 <subhasis4502@gmail.com>
2021-10-29 09:44:21 +0530

    updated json

--- app.js
+++ app.js
@@ -6,28 +6,24 @@ document.getElementById("addBtn").addEventListener("click", function (e) {
   let note = document.getElementById("addTxt");
   let imp = document.getElementById("important");
 
-  let notesTitle = localStorage.getItem("notesTitle");
   let notes = localStorage.getItem("notes");
-  let imps = localStorage.getItem("imp");
 
   if (noteTitle.value.length != 0) {
-    let notesTitleObj = [];
     let notesObj = [];
-    let impObj = [];
 
-    if (notesTitle != null) {
-      notesTitleObj = JSON.parse(notesTitle);
+    if (notes != null) {
       notesObj = JSON.parse(notes);
-      impObj = JSON.parse(imps);
     }
-    notesTitleObj.push(noteTitle.value);
-    notesObj.push(note.value);
-    if (imp.checked) impObj.push("on");
-    else impObj.push("off");
 
-    localStorage.setItem("notesTitle", JSON.stringify(notesTitleObj));
+    let myObj = {
+      title: noteTitle.value,
+      note: note.value,
+      imp: imp.checked ? "on" : "off",
+    };
+
+    notesObj.push(myObj);
+
     localStorage.setItem("notes", JSON.stringify(notesObj));
-    localStorage.setItem("imp", JSON.stringify(impObj));
 
     noteTitle.value = "";
     note.value = "";
@@ -41,47 +37,41 @@ document.getElementById("addBtn").addEventListener("click", function (e) {
 
 function showNotes() {
   let notes = localStorage.getItem("notes");
-  let notesTitle = localStorage.getItem("notesTitle");
-  let imps = localStorage.getItem("imp");
 
   let notesObj = [];
-  let notesTitleObj = [];
-  let impObj = [];
 
-  if (notesTitle != null) {
-    notesTitleObj = JSON.parse(notesTitle);
+  if (notes != null) {
     notesObj = JSON.parse(notes);
-    impObj = JSON.parse(imps);
   }
 
   let html = "";
-  for (let i = 0; i < notesTitleObj.length; i++) {
-    if (impObj[i] == "on") {
+  notesObj.forEach(function (element, index) {
+    if (element.imp == "on") {
       html += `
       <div class="noteCard mx-2 my-2 card" style="width: 18rem">
       <div class="card-body">
-        <h4 class="card-title">⭐ ${notesTitleObj[i]}</h4>
-        <p class="card-text">${notesObj[i]}</p>
-        <button id="${i}" onclick = "deleteNote(this.id)" class="btn btn-primary">Delete Note</button>
+        <h4 class="card-title">⭐ ${element.title}</h4>
+        <p class="card-text">${element.note}</p>
+        <button id="${index}" onclick = "deleteNote(this.id)" class="btn btn-primary">Delete Note</button>
       </div>
     </div>
       `;
     }
-  }
+  });
 
-  for (let i = 0; i < notesTitleObj.length; i++) {
-    if (impObj[i] != "on") {
+  notesObj.forEach(function (element, index) {
+    if (element.imp == "off") {
       html += `
       <div class="noteCard mx-2 my-2 card" style="width: 18rem">
       <div class="card-body">
-        <h4 class="card-title">${notesTitleObj[i]}</h4>
-        <p class="card-text">${notesObj[i]}</p>
-        <button id="${i}" onclick = "deleteNote(this.id)" class="btn btn-primary">Delete Note</button>
+        <h4 class="card-title">${element.title}</h4>
+        <p class="card-text">${element.note}</p>
+        <button id="${index}" onclick = "deleteNote(this.id)" class="btn btn-primary">Delete Note</button>
       </div>
     </div>
       `;
     }
-  }
+  });
 
   let noteElm = document.getElementById("notes");
   if (notesObj.length != 0) noteElm.innerHTML = html;
@@ -95,25 +85,14 @@ function showNotes() {
 //Function to delete notes:
 function deleteNote(index) {
   let notes = localStorage.getItem("notes");
-  let notesTitle = localStorage.getItem("notesTitle");
-  let imps = localStorage.getItem("imp");
-
   let notesObj = [];
-  let notesTitleObj = [];
-  let impObj = [];
 
-  if (notesTitle != null) {
-    notesTitleObj = JSON.parse(notesTitle);
+  if (notes != null) {
     notesObj = JSON.parse(notes);
-    impObj = JSON.parse(imps);
   }
 
   notesObj.splice(index, 1);
-  notesTitleObj.splice(index, 1);
-  impObj.splice(index, 1);
-  localStorage.setItem("notesTitle", JSON.stringify(notesTitleObj));
   localStorage.setItem("notes", JSON.stringify(notesObj));
-  localStorage.setItem("imp", JSON.stringify(impObj));
   showNotes();
 }
 
